4a097ccf3e568eea5e9f59aeda766e27f2fee924
Jon <jbrundagejr@gmail.com>
2021-03-17 16:54:09 -0400

    commit tree b7b64109e708d8f35caca976edb073ed7b72f15b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-
+function superbowlWin(record){
+    let team = record.find(function(element){
+    return element.result === "W"
+    })
+    if (team) {
+        return team.year
+    }
+    else {
+        return undefined
+    }
+}
